abed0f25909384c96f2d735257ea58d8779b28ae
Mike lml <miguelshunidgo@hotmail.com>
2025-04-25 14:58:33 -0600

    fixing attachments text

--- src/components/FilesIndicator.ts
+++ src/components/FilesIndicator.ts
@@ -2,12 +2,15 @@ import { Box, styled } from "@mui/material";
 
 const FilesIndicator = styled(Box)(({ theme }) => ({
   position: "absolute",
-  right: theme.spacing(12),
+  right: 10,
   top: "50%",
   transform: "translateY(-50%)",
   display: "flex",
   alignItems: "center",
   gap: theme.spacing(1),
+  [theme.breakpoints.down(700)]: {
+    top: "75%",
+  },
 }));
 
 export default FilesIndicator;
